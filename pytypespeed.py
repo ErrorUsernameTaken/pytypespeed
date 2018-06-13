02ab4b27b54ce82fdd30d8df5c0d68f47bc8fbb8
ErrorUsernameTaken <tsteller03@icloud.com>
2018-06-13 14:31:08 -0400

    nobody listens

--- pytypespeed.py
+++ pytypespeed.py
@@ -1,48 +1,63 @@
 from time import time
+import random
 i = 0
-prompt = "This is a placeholder prompt." 
+prompt = "This is a placeholder prompt."
+
+#since english-master files are out of order, do something with random module maybe?
+
+#takes user input to calculate difficulty (todo)
+def difficulty():
+    diff = input("Choose a difficulty: Average/Advanced")
+    diff = diff.lower()
+    while True:
+        if diff == 'average':
+            print('The difficulty of your test has been set to average.')
+        elif diff == 'advanced':
+            print('The difficulty of your test has been set to advanced.')
+        else:
+            print('Invalid input.')
+            break
 
 #keeps time
 def counter():
-	i = 0 
-	print(prompt)
-	input("Press enter to begin. Once you have finished, press enter.")
-	begin_time = time()
-	inp = input("\n")
-	end_time = time()
-	final_time = (end_time - begin_time) / 100
-	return final_time, inp
+    i = 0
+    print(prompt)
+    input("Press enter to begin. Once you have finished, press enter.")
+    begin_time = time()
+    inp = input("\n")
+    end_time = time()
+    final_time = (end_time - begin_time) / 100
+    return final_time, inp
 
 #calculates wpm
 def wpm(time, line):
-	words = line.split()
-	word_length = len(words)
-	words_per_m = word_length / time
-	return words_per_m
-    
+    words = line.split()
+    word_length = len(words)
+    words_per_m = word_length / time
+    return words_per_m
+
 #checks if words are correct
 def wordcheck(inp):
-	prompts = prompt.split()
-	inputs = inp.split()
-	errorcount = 0
-	
-	idx = 0
-	for inp in inputs:
-		if inp != prompts[idx]:
-			errorcount += 1
-			if inp == prompts[idx + 1]:
-				idx += 2
-			elif inp != prompts[idx - 1]:
-				idx += 1
-		else:
-			idx += 1
+    prompts = prompt.split()
+    inputs = inp.split()
+    error_count = 0
 
-	words_left = len(prompts) - len(inputs)
-	correct = float(len(prompts)) - float(errorcount)
-	percentage = (((float(correct) / float(len(prompts))) - float(words_left) / float(len(prompts))) * 100)
+    idx = 0
+    for inp in inputs:
+        if inp != prompts[idx]:
+            error_count += 1
+            if inp == prompts[idx + 1]:
+                idx += 2
+            elif inp != prompts[idx - 1]:
+                idx += 1
+        else:
+            idx += 1
 
-	
-	return percentage
+    words_left = len(prompts) - len(inputs)
+    correct = float(len(prompts)) - float(error_count)
+    percentage = (((float(correct) / float(len(prompts))) - float(words_left) / float(len(prompts))) * 100)
+
+    return percentage
 
 #calculates results
 tm, line = counter()
