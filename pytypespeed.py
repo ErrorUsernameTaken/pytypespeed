8ce93ffd7a04b41d245c01c4c45c9c1ef91beb40
ErrorUsernameTaken <tsteller03@icloud.com>
2018-06-20 13:23:30 -0400

    Update pytypespeed.py

--- pytypespeed.py
+++ pytypespeed.py
@@ -1,26 +1,51 @@
 from time import time
 import random
+import os
 i = 0
 prompt = "This is a placeholder prompt."
 
 #since english-master files are out of order, do something with random module maybe?
 
-#takes user input to calculate difficulty (todo)
-def difficulty():
-    diff = input("Choose a difficulty: Average/Advanced")
-    diff = diff.lower()
-    while True:
-        if diff == 'average':
-            print('The difficulty of your test has been set to average.')
-        elif diff == 'advanced':
-            print('The difficulty of your test has been set to advanced.')
+#file exists but it says there's no path. learn more (todo)
+#have the user control how many words they type (todo)
+#fix some errors with typing (todo)
+"""
+This is aplceholder repmopt. -
+  File "/Users/Trent/Desktop/Programming/Python/!!!WIP Projects/TypingTest.py", line 83, in <module>
+    words_per_minute = wpm(tm, line)
+  File "/Users/Trent/Desktop/Programming/Python/!!!WIP Projects/TypingTest.py", line 54, in wpm
+    words_per_m = word_length / time
+ZeroDivisionError: float division by zero
+"""
+
+diff = input("Choose a difficulty: Average/Advanced ")
+diff = diff.lower()
+if diff == 'average':
+    print('The difficulty of your test has been set to average.')
+elif diff == 'advanced':
+    print('The difficulty of your test has been set to advanced.')
+else:
+    print('Invalid input.')
+
+#checks if the user has the files
+def filecheck():
+    if diff == 'average':
+        if os.path.exists("10k-english-usa-no-swears-medium.txt"):
+            #run prompts
+            print("h")
         else:
-            print('Invalid input.')
-            break
+            print("The file does not exist. \nPlease download the file here - https://bit.ly/2M8205X")
+    elif diff == 'advanced':
+        if os.path.exists("10k-english-usa-no-swears-long.txt"):
+            #run prompts
+            print("h")
+        else:
+            print("The file does not exist. \nPlease download the file here - https://bit.ly/2M8205X")
+
+filecheck()
 
 #keeps time
 def counter():
-    i = 0
     print(prompt)
     input("Press enter to begin. Once you have finished, press enter.")
     begin_time = time()
