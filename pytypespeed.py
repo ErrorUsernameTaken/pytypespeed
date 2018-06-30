aa07c1b92c74c532e4c5f1344290d55aa6649add
ErrorUsernameTaken <tsteller03@icloud.com>
2018-06-30 16:00:42 -0400

    Update pytypespeed.py

--- pytypespeed.py
+++ pytypespeed.py
@@ -1,6 +1,11 @@
-from time import time
-import random
+from time import time #removing from time breaks the program for some reason
+#import average-prompt
+#import advanced-prompt
+#import random
+#import keyboard
 import os
+import sys
+
 prompt = "This is a placeholder prompt."
 
 #since english-master files are out of order, do something with random module maybe?
@@ -9,58 +14,91 @@ prompt = "This is a placeholder prompt."
 #fix accuracy and wpm (todo)
 #how do i test if wpm is correcT?
 #fix calculating errors (todo)
-#okay man what am i going to do if things go wrong?
-#i need functions.
+#prevent backspaces (either use pynput or keyboard module)
 
 #Errors -
 """
 This is aplceholder repmopt. -
-  File "/Users/Trent/Desktop/Programming/Python/!!!WIP Projects/TypingTest.py", line 83, in <module>
-    words_per_minute = wpm(tm, line)
-  File "/Users/Trent/Desktop/Programming/Python/!!!WIP Projects/TypingTest.py", line 54, in wpm
-    words_per_m = word_length / time
+File "/Users/Trent/Desktop/Programming/Python/!!!WIP Projects/TypingTest.py", line 83, in <module>
+words_per_minute = wpm(tm, line)
+File "/Users/Trent/Desktop/Programming/Python/!!!WIP Projects/TypingTest.py", line 54, in wpm
+words_per_m = word_length / time
 ZeroDivisionError: float division by zero
 
 this is a placehodler prompt
-  File "/Users/Trent/Desktop/Programming/Python/!!!WIP Projects/TypingTest.py", line 125, in <module>
-    percentage = wordcheck(line)
-  File "/Users/Trent/Desktop/Programming/Python/!!!WIP Projects/TypingTest.py", line 105, in wordcheck
-    if inp == prompts[idx + 1]:
+File "/Users/Trent/Desktop/Programming/Python/!!!WIP Projects/TypingTest.py", line 125, in <module>
+percentage = wordcheck(line)
+File "/Users/Trent/Desktop/Programming/Python/!!!WIP Projects/TypingTest.py", line 105, in wordcheck
+if inp == prompts[idx + 1]:
 IndexError: list index out of range
 """
 
-diff = input("Choose a difficulty: Average/Advanced ")
-diff = diff.lower()
-if diff == 'average':
-    print('The difficulty of your test has been set to average.')
-elif diff == 'advanced':
-    print('The difficulty of your test has been set to advanced.')
-else:
-    print('Invalid input.')
+print("Notice: backspaces are disabled. (or will be)\n")
+
+"""
+while True:
+
+"""
+def choices():
+    global diff
+    diff = input("Choose a difficulty: Average/Advanced ")
+    diff = diff.lower()
+    x = 0
+    while x > 1:
+        if diff == 'average':
+            x = x + 1
+        elif diff == 'advanced':
+            x = x + 1
+
+    if diff not in ('average', 'advanced'):
+        print('Your input is invalid. Please try again.')
+        choices()
+
+choices()
+
+def deathchoice():
+    global death
+    death = input("Would you like sudden death to be enabled? (Y/N) ")
+    death = death.lower()
+    z = 0
+    while z > 1:
+        if death == 'y':
+            z = z + 1
+        elif death == 'n':
+            z = z + 1
+
+    if death not in ('y', 'n'):
+        print('Your input is invalid. Please try again.')
+        deathchoice()
+
+deathchoice()
 
 #checks if the user has the files
 #maybe use try/catch statement?
 
-filepath = input("Where is the prompt file located?: ")
-#personal is /Users/Trent/Desktop/Future\ Reference/google-10000-english-master/10k-english-usa-no-swears-medium.txt
-#/Users/Trent/Desktop/Future\ Reference/google-10000-english-master/10k-english-usa-no-swears-long.txt
-#I DON'T KNOW HOW TO DO THIS ANY OTHER WAY =)
-if diff == 'average':
-    if os.path.exists(filepath):
-        #run prompts (call counter() )
-        #seems to work without calling counter(), which is interesting. might change later
-        print("h")
-    else:
-        print("The file does not exist. \nPlease download the file here - https://bit.ly/2M8205X")
-        print("Alternatively, if you have already downloaded the file, please restart the program and try again.")
-elif diff == 'advanced':
-    if os.path.exists(filepath):
-        #run prompts (call counter() )
-        print("h")
-    else:
-        print("The file does not exist. \nPlease download the file here - https://bit.ly/2M8205X")
-        print("Alternatively, if you have already downloaded the file, please restart the program and try again.")
+def file():
+    global filepath
+    filepath = input("Where is the prompt file located?: ")
+    #personal is /Users/Trent/Desktop/Future\ Reference/google-10000-english-master/10k-english-usa-no-swears-medium.txt
+    #/Users/Trent/Desktop/Future\ Reference/google-10000-english-master/10k-english-usa-no-swears-long.txt
+    #I DON'T KNOW HOW TO DO THIS ANY OTHER WAY =)
+    if diff == 'average':
+        if os.path.exists(average-prompt):
+            #run prompts (call counter() )
+            #seems to work without calling counter(), which is interesting. might change later
+            print("h")
+        else:
+            print("The file does not exist. \nPlease download the file here - https://bit.ly/2M8205X")
+            print("Alternatively, if you have already downloaded the file, please restart the program and try again.")
+    elif diff == 'advanced':
+        if os.path.exists(advanced-prompt):
+            #run prompts (call counter() )
+            print("h")
+        else:
+            print("The file does not exist. \nPlease download the file here - https://bit.ly/2M8205X")
+            print("Alternatively, if you have already downloaded the file, please restart the program and try again.")
 
+file()
 
 #keeps track of the user's word amount
 """
