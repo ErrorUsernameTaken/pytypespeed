d47d20a3be98f985758e37d4cc938e0afd6a9010
ErrorUsernameTaken <tsteller03@icloud.com>
2018-10-07 14:05:25 -0400

    properly formatted time + added ideas

--- pytypespeed.py
+++ pytypespeed.py
@@ -1,13 +1,48 @@
-from time import time
-prompt = "hey, this is a placeholder"
+import time
 
+prompt = "walmart gaming yeah" #placeholder prompt
+
+#lets the user choose the word difficulty
+def diffChoice():
+        diffi = input("Average or advanced?: ").lower()
+        if diffi not in ('average', 'advanced'):
+                print("Please pick a valid difficulty.")
+                diffChoice()
+        else:
+                if diffi == 'average':
+                        print("advanced")
+                        #open average-prompt.py
+                        #and then, when the user picks the word amount
+                        #^ call wordChoice for this
+                        #select that many words from that file
+                elif diffi == 'advanced':
+                        print("advanced")
+                        #same thing, different files
+
+diffChoice()
+
+#lets the user choose how many words they want to type
+def wordChoice():
+        wordAmt = int(input("How many words do you want to type? (50-1000): "))
+        if wordAmt not in range(50, 1000):
+                print("Please pick a valid number.")
+                wordChoice()
+        else:
+                print(wordAmt)
+                #select wordAmt words from whatever file
+                #need to print out the words so that they aren't
+                #in a list format
+        
+#keeps track of time
 def counter():
 	print(prompt)
 	input(">> Press ENTER to begin")
-	begin_time = time()
+	global start_time
+	start_time = time.time()
 	inp = input("\n")
-	end_time = time()
-	final_time = (end_time - begin_time) / 60
+	global end_time
+	end_time = time.time()
+	final_time = (end_time - start_time) / 60
 	return final_time, inp
 
 #calculates words per minute
@@ -17,7 +52,7 @@ def calcWpm(time, line):
 	words_per_m = word_length / time
 	return words_per_m
 
-
+#checks for incorrect works
 def wordcheck(inp):
 	prompts = prompt.split()
 	inputs = inp.split()
@@ -44,9 +79,11 @@ def wordcheck(inp):
 
 tm, line = counter()
 tm = round(tm, 2)
+hours, rem = divmod(end_time - start_time, 3600)
+minutes, seconds = divmod(rem, 60)
 wpm = calcWpm(tm, line)
 wpm = round(wpm, 2)
-print("Total time: {} minutes".format(tm))
+print("Total time: {:0>2}:{:05.2f}".format(int(minutes),seconds))
 print("Average WPM: {}".format(wpm))
 percentage = wordcheck(line)
 percentager = round(percentage, 2)
